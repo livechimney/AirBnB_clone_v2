0751fcdc4a13f02d89350b715f45866ff437ba6f
livechimney <adrianmuriuki1366@gmail.com>
2024-04-18 18:11:46 +0300

    v2 claen up

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" """
+"""basemodel  """
 from models.base_model import BaseModel
 import unittest
 import datetime
@@ -7,6 +7,7 @@ from uuid import UUID
 import json
 import os
 import pycodestyle
+import BaseModel
 
 
 class test_basemodel(unittest.TestCase):
