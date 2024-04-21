3fc3aaf5a9cac0d59b49df46602018236727426f
livechimney <adrianmuriuki1366@gmail.com>
2024-04-21 21:57:15 +0300

    web flask

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -25,11 +25,11 @@ def hbnb():
 
 
 @app.route("/c/<text>", strict_slashes=False)
-def c(text):
+def c_is_fun(text):
     """Displays 'C' followed by the value of <text>."""
     text = text.replace("_", " ")
     return "C {}".format(text)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
