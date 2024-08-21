e73418b05c0e52ab312ed4c1e06e25dd46927f02
livechimney <adrianmuriuki1366@gmail.com>
2024-08-21 10:22:17 +0000

    v2

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,29 +1,15 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 """
     Sript that starts a Flask web application
  """
-=======
-<<<<<<< HEAD
 """
 start Flask application
 """
 
->>>>>>> 2f0813143d9d93724458ff791ae5501a83b7fc94
 from flask import Flask
 app = Flask(__name__)
 
 
-<<<<<<< HEAD
-@app.route('/', strict_slashes=False)
-def hello_hbn():
-    """
-        function to return Hello HBNB!
-    """
-    return "Hello HBNB!"
-if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=5000)
-=======
 @app.route('/airbnb-onepage/', strict_slashes=False)
 def index():
     """returns Hello HBNB!"""
@@ -31,7 +17,6 @@ def index():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
-=======
 """Starts a Flask web application.
 
 The application listens on 0.0.0.0, port 5000.
@@ -51,5 +36,4 @@ def hello_hbnb():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
->>>>>>> 43a24faa69bbd151b8fec987ee56df7de02c39b7
->>>>>>> 2f0813143d9d93724458ff791ae5501a83b7fc94
+
